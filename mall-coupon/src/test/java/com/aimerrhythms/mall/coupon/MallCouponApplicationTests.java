90e21643850184ba2c1722c82582be2d62be6388
aimerrhythms <nishimiyaskii@163.com>
2020-05-28 01:21:38 +0800

    项目初始结构创建

---
+++ mall-coupon/src/test/java/com/aimerrhythms/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimerrhythms.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
